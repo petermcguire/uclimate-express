de91b1518f810b3670de589cd90b7927079ad704
Peter McGuire <petermcgu@gmail.com>
2023-12-09 19:46:26 -0800

    updated and brought into WebStorm

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var homesRouter = require('./routes/homes');
+const indexRouter = require('./routes/index');
+const homesRouter = require('./routes/homes');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
